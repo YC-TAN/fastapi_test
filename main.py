ed8fc5e8f68fa25a1b4adce6e253bc35f3c3ff9d
Yin <150326068+YC-TAN@users.noreply.github.com>
2025-03-11 11:51:44 +1300

    Update main.py
    Add database connection

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from datetime import datetime, timezone
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, Query
 from pydantic import EmailStr
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from typing import Annotated
 
 
 class User(SQLModel, table=True):
@@ -13,9 +14,36 @@ class User(SQLModel, table=True):
     is_disabled: bool = Field(default=False)
 
 
+sqlite_file_name = "test.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def drop_all_tables():
+    SQLModel.metadata.drop_all(engine)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.on_event("startup")
+def on_startup():
+    # drop_all_tables() # Uncomment this line to drop all existing tables
+    create_db_and_tables() # Ensure all tables exists
