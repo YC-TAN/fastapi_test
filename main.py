9a0cabe3130332723bff896ec61d24a312ff7731
Yin <150326068+YC-TAN@users.noreply.github.com>
2025-03-10 16:30:14 +1300

    Update main.py
    Add User model

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+from datetime import datetime, timezone
 from fastapi import FastAPI
+from pydantic import EmailStr
+from sqlmodel import Field, SQLModel
+
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    email: EmailStr = Field(unique=True)
+    hashed_password: str
+    role: str
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    is_disabled: bool = Field(default=False)
 
 
 app = FastAPI()
