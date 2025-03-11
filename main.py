c04aff47606f9967f5880920c55eef39b72771da
Yin <150326068+YC-TAN@users.noreply.github.com>
2025-03-11 15:44:43 +1300

    Update main.py
    Add initial crud requests

--- main.py
+++ main.py
@@ -29,7 +29,44 @@ app = FastAPI()
 def on_startup():
     drop_all_tables() # Uncomment this line to drop all existing tables
     create_db_and_tables() # Ensure all tables exists
+    
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/users/")
+def create_user(user: User, session: SessionDep) -> User:
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
+
+
+@app.get("/users/")
+def read_users(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> list[User]:
+    users = session.exec(select(User).offset(offset).limit(limit)).all()
+    return users
+
+
+@app.get("/users/{user_id}")
+def read_user(user_id: int, session: SessionDep) -> User:
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="user not found")
+    return user
+
+
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int, session: SessionDep):
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="user not found")
+    session.delete(user)
+    session.commit()
+    return {"ok": True}
