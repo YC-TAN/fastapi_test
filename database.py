78cb574424fb33522a530fb98b569065439dd266
Yin <150326068+YC-TAN@users.noreply.github.com>
2025-03-14 16:45:12 +1300

    Update database.py
    Change database to Postgres

--- database.py
+++ database.py
@@ -6,12 +6,20 @@ import os
 # Load environment variables from .env file
 load_dotenv()
 
-# sqlite_file_name = "test.db"
-file_name = os.getenv("SQL_FILE_NAME")
-sqlite_url = f"sqlite:///{file_name}"
+secret_key = os.getenv("SECRET_KEY")
 
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+DB_NAME = "test_user"
+db_url = "postgresql://postgres:test1234@localhost:5432/test_user"
+DATABASE_URL = "postgresql+asyncpg://postgres:test1234@localhost:5432/test_user"
+
+engine = create_engine(db_url, echo=True)
+
+# sqlite_file_name = "test.db"
+# file_name = os.getenv("SQL_FILE_NAME")
+# sqlite_url = f"sqlite:///{file_name}"
+
+# connect_args = {"check_same_thread": False}
+# engine = create_engine(sqlite_url, connect_args=connect_args)
 
 
 def drop_all_tables():
